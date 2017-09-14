018488cf4a89f9ba6a0794a3af9bd982dfbb4d21
ssimilae <ssimilae@gmail.com>
2017-09-14 15:22:50 +0900

    1
    2

--- src/android/InAppChromeClient.java
+++ src/android/InAppChromeClient.java
@@ -40,151 +40,59 @@ import android.view.MotionEvent;
 import android.view.WindowManager;
 
 public class InAppChromeClient extends WebChromeClient {
-
-    private CordovaWebView webView;
-
-	private View mCustomView;
-    private Activity mActivity;
-
-
-
-    private String LOG_TAG = "InAppChromeClient";
-    private long MAX_QUOTA = 100 * 1024 * 1024;
-
-    public InAppChromeClient(CordovaWebView webView, Activity activity) {
-        super();
-        this.webView = webView; 
+    protected Activity mActivity = null;
+ 
+    private View mCustomView;
+    private WebChromeClient.CustomViewCallback mCustomViewCallback;
+    private int mOriginalOrientation;
+ 
+    private FrameLayout mContentView;
+    private FrameLayout mFullscreenContainer;
+ 
+    private static final FrameLayout.LayoutParams COVER_SCREEN_PARAMS = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+ 
+    public InAppChromeClient(Activity activity) {
         this.mActivity = activity;
-  
     }
-    /**
-     * Handle database quota exceeded notification.
-     *
-     * @param url
-     * @param databaseIdentifier
-     * @param currentQuota
-     * @param estimatedSize
-     * @param totalUsedQuota
-     * @param quotaUpdater
-     */
+ 
     @Override
-    public void onExceededDatabaseQuota(String url, String databaseIdentifier, long currentQuota, long estimatedSize,
-            long totalUsedQuota, WebStorage.QuotaUpdater quotaUpdater)
-    {
-        LOG.d(LOG_TAG, "onExceededDatabaseQuota estimatedSize: %d  currentQuota: %d  totalUsedQuota: %d", estimatedSize, currentQuota, totalUsedQuota);
-        quotaUpdater.updateQuota(MAX_QUOTA);
-    }
-
-    /**
-     * Instructs the client to show a prompt to ask the user to set the Geolocation permission state for the specified origin.
-     *
-     * @param origin
-     * @param callback
-     */
-    @Override
-    public void onGeolocationPermissionsShowPrompt(String origin, Callback callback) {
-        super.onGeolocationPermissionsShowPrompt(origin, callback);
-        callback.invoke(origin, true, false);
-    }
-
-    /**
-     * Tell the client to display a prompt dialog to the user.
-     * If the client returns true, WebView will assume that the client will
-     * handle the prompt dialog and call the appropriate JsPromptResult method.
-     *
-     * The prompt bridge provided for the InAppBrowser is capable of executing any
-     * oustanding callback belonging to the InAppBrowser plugin. Care has been
-     * taken that other callbacks cannot be triggered, and that no other code
-     * execution is possible.
-     *
-     * To trigger the bridge, the prompt default value should be of the form:
-     *
-     * gap-iab://<callbackId>
-     *
-     * where <callbackId> is the string id of the callback to trigger (something
-     * like "InAppBrowser0123456789")
-     *
-     * If present, the prompt message is expected to be a JSON-encoded value to
-     * pass to the callback. A JSON_EXCEPTION is returned if the JSON is invalid.
-     *
-     * @param view
-     * @param url
-     * @param message
-     * @param defaultValue
-     * @param result
-     */
-    @Override
-    public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {
-        // See if the prompt string uses the 'gap-iab' protocol. If so, the remainder should be the id of a callback to execute.
-        if (defaultValue != null && defaultValue.startsWith("gap")) {
-            if(defaultValue.startsWith("gap-iab://")) {
-                PluginResult scriptResult;
-                String scriptCallbackId = defaultValue.substring(10);
-                if (scriptCallbackId.startsWith("InAppBrowser")) {
-                    if(message == null || message.length() == 0) {
-                        scriptResult = new PluginResult(PluginResult.Status.OK, new JSONArray());
-                    } else {
-                        try {
-                            scriptResult = new PluginResult(PluginResult.Status.OK, new JSONArray(message));
-                        } catch(JSONException e) {
-                            scriptResult = new PluginResult(PluginResult.Status.JSON_EXCEPTION, e.getMessage());
-                        }
-                    }
-                    this.webView.sendPluginResult(scriptResult, scriptCallbackId);
-                    result.confirm("");
-                    return true;
-                }
+    public void onShowCustomView(View view, int requestedOrientation, WebChromeClient.CustomViewCallback callback) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            if (mCustomView != null) {
+                callback.onCustomViewHidden();
+                return;
             }
-            else
-            {
-                // Anything else with a gap: prefix should get this message
-                LOG.w(LOG_TAG, "InAppBrowser does not support Cordova API calls: " + url + " " + defaultValue); 
-                result.cancel();
-                return true;
-            }
+ 
+            mOriginalOrientation = mActivity.getRequestedOrientation();
+            FrameLayout decor = (FrameLayout) mActivity.getWindow().getDecorView();
+            mFullscreenContainer = new FullscreenHolder(mActivity);
+            mFullscreenContainer.addView(view, COVER_SCREEN_PARAMS);
+            decor.addView(mFullscreenContainer, COVER_SCREEN_PARAMS);
+            mCustomView = view;
+            setFullscreen(true);
+            mCustomViewCallback = callback;
+            mActivity.setRequestedOrientation(requestedOrientation);
         }
-        return false;
+ 
+        super.onShowCustomView(view, requestedOrientation, callback);
     }
-
-	@Override
-    public void onCloseWindow(WebView window) {
-         super.onCloseWindow(window);
+ 
+    @Override
+    public void onHideCustomView() {
+        if (mCustomView == null) {
+            return;
+        }
+ 
+        setFullscreen(false);
+        FrameLayout decor = (FrameLayout) mActivity.getWindow().getDecorView();
+        decor.removeView(mFullscreenContainer);
+        mFullscreenContainer = null;
+        mCustomView = null;
+        mCustomViewCallback.onCustomViewHidden();
+        mActivity.setRequestedOrientation(mOriginalOrientation);
     }
-
-	
-
-
-	 private int mOriginalOrientation;
-	 private FrameLayout mContentView;
  
-     private FullscreenHolder mFullscreenContainer;
-     private CustomViewCallback mCustomViewCollback;
- 
-     @Override
-     public void onShowCustomView(View view, CustomViewCallback callback) {
- 
-         if (mCustomView != null) {
-             callback.onCustomViewHidden();
-             return;
-         }
- 
-         mOriginalOrientation = mActivity.getRequestedOrientation();
- 
-         FrameLayout decor = (FrameLayout) mActivity.getWindow().getDecorView();
- 
-         mFullscreenContainer = new FullscreenHolder(mActivity);
-         mFullscreenContainer.addView(view, ViewGroup.LayoutParams.MATCH_PARENT);
-         decor.addView(mFullscreenContainer, ViewGroup.LayoutParams.MATCH_PARENT);
-         mCustomView = view;
-		  setFullscreen(true);
-
-         mCustomViewCollback = callback;
-         mActivity.setRequestedOrientation(mOriginalOrientation);
-		super.onShowCustomView(view, mOriginalOrientation, callback);
-
-     }	
-	
-	  private void setFullscreen(boolean enabled) {
+    private void setFullscreen(boolean enabled) {
         Window win = mActivity.getWindow();
         WindowManager.LayoutParams winParams = win.getAttributes();
         final int bits = WindowManager.LayoutParams.FLAG_FULLSCREEN;
@@ -200,36 +108,17 @@ public class InAppChromeClient extends WebChromeClient {
         }
         win.setAttributes(winParams);
     }
-
  
-
-     @Override
-     public void onHideCustomView() {
-         if (mCustomView == null) {
-             return;
-         }
+    private static class FullscreenHolder extends FrameLayout {
+        public FullscreenHolder(Context ctx) {
+            super(ctx);
+            setBackgroundColor(ctx.getResources().getColor(android.R.color.black));
+        }
  
-         FrameLayout decor = (FrameLayout) mActivity.getWindow().getDecorView();
-         decor.removeView(mFullscreenContainer);
-         mFullscreenContainer = null;
-         mCustomView = null;
-         mCustomViewCollback.onCustomViewHidden();
-    
-         mActivity.setRequestedOrientation(mOriginalOrientation);
-     }
- 
- 
-     static class FullscreenHolder extends FrameLayout {
- 
-         public FullscreenHolder(Context ctx) {
-             super(ctx);
-             setBackgroundColor(ctx.getResources().getColor(android.R.color.black));
-         }
-    
-         @Override
-         public boolean onTouchEvent(MotionEvent evt) {
-             return true;
-         }
+        @Override
+        public boolean onTouchEvent(MotionEvent evt) {
+            return true;
+        }
     }
-
 }
+ 
