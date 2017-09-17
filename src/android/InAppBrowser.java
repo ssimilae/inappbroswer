eaf69262dc8a2a79dbe72196cfd10b49c86b6d49
ssimilae <ssimilae@gmail.com>
2017-09-17 21:53:40 +0900

    1
    2

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -729,7 +729,7 @@ public class InAppBrowser extends CordovaPlugin {
                 inAppWebView.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
                 inAppWebView.setId(Integer.valueOf(6));
                 // File Chooser Implemented ChromeClient
-                inAppWebView.setWebChromeClient(new InAppChromeClient(cordova.getActivity()) {
+                inAppWebView.setWebChromeClient(new InAppChromeClient(thatWebView,cordova.getActivity()) {
                     // For Android 5.0+
                     public boolean onShowFileChooser (WebView webView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams)
                     {
